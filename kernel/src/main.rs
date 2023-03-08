793c965358d98b120b1fe8493c9a2e36f825b263
Celve <celve03@gmail.com>
2023-03-08 15:01:24 +0800

    fix(bootloader): move the sp to the correct address of the stack when starting up

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,9 +1,12 @@
 #![no_std]
 #![no_main]
-#![feature(naked_functions)]
+#![feature(naked_functions, asm_const)]
+
+// extern crate alloc;
 
 mod complement;
 mod config;
+// mod heap;
 mod io;
 
 use config::*;
@@ -19,10 +22,13 @@ static mut BOOTLOADER_STACK: [u8; BOOTLOADER_STACK_SIZE] = [0; BOOTLOADER_STACK_
 unsafe extern "C" fn _start() {
     asm!(
         "la sp, {bootloader_stack}",
+        "li t0, {bootloader_stack_size}",
+        "add sp, sp, t0",
         "j {rust_main}",
         bootloader_stack = sym BOOTLOADER_STACK,
+        bootloader_stack_size = const BOOTLOADER_STACK_SIZE,
         rust_main = sym rust_main,
-        options(noreturn)
+        options(noreturn),
     );
 }
 

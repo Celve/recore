4ecce349c302e6f27d50325505fdf33b6ee781fd
Celve <celve03@gmail.com>
2023-03-16 22:27:32 +0800

    refactor(mm): remove global symbol trampoline

--- kernel/src/trap/trampoline.rs
+++ kernel/src/trap/trampoline.rs
@@ -1,12 +1,4 @@
-use core::arch::{asm, global_asm};
-
-use alloc::sync::Arc;
-use lazy_static::lazy_static;
-
-use crate::{
-    mm::{address::VirAddr, area::Area, memory::MappingPermission},
-    sync::up::UpCell,
-};
+use core::arch::global_asm;
 
 global_asm!(include_str!("trampoline.s"));
 
@@ -19,18 +11,3 @@ global_asm!(include_str!("trampoline.s"));
 // "    ld x\n, \n*8(sp)",
 // ".endm"
 // );
-
-lazy_static! {
-    pub static ref TRAMPOLINE: Arc<UpCell<Area>> = {
-        extern "C" {
-            fn strampoline();
-            fn etrampoline();
-        }
-        let area = Area::new_identical(
-            VirAddr::from(strampoline as usize).floor_to_vir_page_num(),
-            VirAddr::from(etrampoline as usize).ceil_to_vir_page_num(),
-            MappingPermission::R | MappingPermission::X,
-        );
-        Arc::new(UpCell::new(area))
-    };
-}
